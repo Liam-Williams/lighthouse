d7f39792abaaac5a21a3dd5f55a89f716b01defa
jackiechong <jackiekchong@mail.com>
2015-09-22 12:14:52 -0400

    Added maximum assignment

---
+++ week1/day2/maximum/max.rb
@@ -0,0 +1,23 @@
+# Find the maximum 
+def maximum(arr)
+  largest = arr.first
+    arr.each do |current_largest|
+      if current_largest > largest
+        return current_largest
+      end
+    end
+end
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"
